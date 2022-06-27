f4a2034b7ff9b22f384df0a2ba803dbd26bbaef1
avatonk1 <89200617+avatonk1@users.noreply.github.com>
2022-06-28 00:24:47 +0300

    Update installg.sh

--- installg.sh
+++ installg.sh
@@ -10,7 +10,7 @@ echo 'export NODENAME='$NODENAME_GEAR >> $HOME/.profile
 echo "-----------------------------------------------------------------------------"
 echo "Устанавливаем софт"
 echo "-----------------------------------------------------------------------------"
-curl -s https://raw.githubusercontent.com/razumv/helpers/main/tools/install_ufw.sh | bash &>/dev/null
+curl -s https://raw.githubusercontent.com/avatonk1/Gear/main/ufw.sh | bash &>/dev/null
 curl -s https://raw.githubusercontent.com/avatonk1/Gear/main/rust.sh | bash &>/dev/null
 sudo apt install --fix-broken -y &>/dev/null
 sudo apt install nano mc git mc clang curl jq htop net-tools libssl-dev llvm libudev-dev -y &>/dev/null
